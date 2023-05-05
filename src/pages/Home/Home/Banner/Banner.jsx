dba53bec29a9153dcdbe8916da3b692502812cae
Md.Mohiuddin <saddammd203@gmail.com>
2023-05-05 22:21:15 +0600

    added slider title

--- src/pages/Home/Home/Banner/Banner.jsx
+++ src/pages/Home/Home/Banner/Banner.jsx
@@ -6,37 +6,37 @@ function Banner() {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
+          src="/src/assets/tony7755109801519956930.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3>Bawarchi</h3>
+          <p>Food Preparation: Chefs are responsible for preparing food according to recipes and standards set by their establishment. They may also be responsible for ordering ingredients, maintaining inventory, and ensuring food safety.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
+          src="/src/assets/20190328_140907_0000-2.png"
           alt="Second slide"
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h3>Ranna Master</h3>
+          <p>Menu Planning: Chefs are often responsible for creating menus that meet the needs and preferences of their customers. They may also need to take into account factors such as seasonality, food availability, and budget.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
+          src="/src/assets/img_20200809_192445.jpg"
           alt="Third slide"
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <h3>Pitha Master</h3>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          Cooking: Chefs use a variety of cooking techniques to prepare food, including grilling, sautéing, roasting, and baking. They must be able to cook food to the proper temperature and ensure that it is cooked evenly and consistently.
           </p>
         </Carousel.Caption>
       </Carousel.Item>
